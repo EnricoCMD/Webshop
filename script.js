9b7a8ae00bc6c45b10d7c13294de786962c484fd
EnricoCMD <78848058+EnricoCMD@users.noreply.github.com>
2024-05-01 00:05:21 +0200

    späti_enrico

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.querySelector('.person').addEventListener('click', function() {
 document.querySelector('.cart').addEventListener('click', function() {
     window.location.href = 'Warenkorb.html';
 });
+
+// Selektiere das Element
+const werbung = document.getElementById('werbung');
+
+// Füge den Eventlistener hinzu
+werbung.addEventListener('mouseenter', function() {
+    // Ändere die Hintergrundfarbe auf Schwarz
+    this.style.backgroundColor = 'black';
+});
+
+// Füge den Eventlistener hinzu, um die ursprüngliche Hintergrundfarbe wiederherzustellen
+werbung.addEventListener('mouseleave', function() {
+    // Ändere die Hintergrundfarbe auf Transparent
+    this.style.backgroundColor = 'rgb(158, 158, 158)';
+});
