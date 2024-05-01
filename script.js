8763c5ec49a8a51eccc3bc65d279baabb505bc96
EnricoCMD <78848058+EnricoCMD@users.noreply.github.com>
2024-05-01 14:11:34 +0200

    tag des mindestlohn

--- script.js
+++ script.js
@@ -12,19 +12,24 @@ document.querySelector('.icon.heart').addEventListener('click', function() {
 
 
 
-// Das Skript ändert die Farbe des Textes im Element "werbung" alle 0,2 Sekunden mit einer zufälligen Verzögerung von bis zu 0,1 Sekunden.
+// Das Skript ändert die Farbe des Textes
 setInterval(function() {
     var werbung = document.getElementById('werbung');
     var currentColor = window.getComputedStyle(werbung).color;
     
     if (currentColor === 'rgb(255, 0, 0)') {
-        werbung.style.color = '#000'; // Textfarbe auf Schwarz ändern, wenn sie Rot ist
+        werbung.style.color = '#ffffff'; // Textfarbe auf weiß ändern, wenn sie Rot ist
     } else {
-        werbung.style.color = '#ff0000'; // Textfarbe auf Rot ändern, wenn sie Schwarz ist
+        werbung.style.color = '#ff0000'; // Textfarbe auf Rot ändern, wenn sie weiß ist
     }
-}, Math.random() < 0.5 ? 250 : 2500 + Math.random() * 200); // Entweder alle 0,25 Sekunden oder nach einer zufälligen Verzögerung von bis zu 2,5 Sekunden
+}, Math.random() < 0.5 ? 250 : 500 + Math.random() * 200); // Entweder alle 0,25 Sekunden oder nach einer zufälligen Verzögerung von bis zu 2,5 Sekunden
 
 
+//fotos kommen von unten nach oben
 
-
-
+document.addEventListener('DOMContentLoaded', function() {
+    var products = document.querySelectorAll('.product');
+    products.forEach(function(product) {
+        product.classList.add('active');
+    });
+});
