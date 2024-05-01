63abeee759814187e6cd10d2f95f992fddee067e
EnricoCMD <78848058+EnricoCMD@users.noreply.github.com>
2024-05-01 13:12:38 +0200

    tag des mindestlohn

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-// person click event auf andere seite // SELECTOR MUSS DURCH DIE AKTUELLE KLASSE ERSETZT WERDEN
-document.querySelector('.icon person').addEventListener('click', function() {
+// person click event auf andere seiten
+document.querySelector('.icon.person').addEventListener('click', function() {
     window.location.href = 'profile.html';
 });
 
-document.querySelector('.icon cart').addEventListener('click', function() {
+document.querySelector('.icon.cart').addEventListener('click', function() {
     window.location.href = 'Warenkorb.html';
 });
-document.querySelector('.icon heart').addEventListener('click', function() {
+document.querySelector('.icon.heart').addEventListener('click', function() {
     window.location.href = 'Favoriten.html';
 });
 
 
-// Das Script ändert die Schriftfarbe des Textes im Intervall von 1 Sekunde zwischen Rot und Schwarz.
+
+// Das Skript ändert die Farbe des Textes im Element "werbung" alle 0,2 Sekunden mit einer zufälligen Verzögerung von bis zu 0,1 Sekunden.
 setInterval(function() {
     var werbung = document.getElementById('werbung');
     var currentColor = window.getComputedStyle(werbung).color;
-    if (currentColor === 'rgb(255, 0, 0);') {
-        werbung.style.color = 'rgb(0, 0, 0)';
+    
+    if (currentColor === 'rgb(255, 0, 0)') {
+        werbung.style.color = '#000'; // Textfarbe auf Schwarz ändern, wenn sie Rot ist
     } else {
-        werbung.style.color = 'rgb(255, 0, 0)';
+        werbung.style.color = '#ff0000'; // Textfarbe auf Rot ändern, wenn sie Schwarz ist
     }
-}, 1000); // 1000 Millisekunden = 1 Sekunde
+}, Math.random() < 0.5 ? 250 : 2500 + Math.random() * 200); // Entweder alle 0,25 Sekunden oder nach einer zufälligen Verzögerung von bis zu 2,5 Sekunden
+
+
+
+
 
